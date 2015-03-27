58f038afca4052634f00a1e4ebb26316efd48291
susinda <susinda@wso2.com>
2015-03-27 16:06:56 +0530

    namespaces added

--- src/main/java/org/susi/Location.java
+++ src/main/java/org/susi/Location.java
@@ -11,14 +11,14 @@ public class Location{
 	String lon;
 
 	@WebMethod(operationName = "update")
-	public String update(@WebParam(name = "id") String id, @WebParam(name = "lat") String lat, @WebParam(name = "lon") String lon) {
+	public String update(@WebParam(name = "id", targetNamespace="http://susi.org") String id, @WebParam(name = "lat", targetNamespace="http://susi.org") String lat, @WebParam(name = "lon", targetNamespace="http://susi.org") String lon) {
 		this.lat = lat;
 		this.lon = lon;
 		return "Updated lon " +  lon + " lat " + lat ;
 	}
 	
 	@WebMethod(operationName = "view")
-	public String view(@WebParam(name = "id") String id) {
+	public String view(@WebParam(name = "id", targetNamespace="http://susi.org") String id) {
 		return "("+ lat + "," + lon +")";
 	}
 }
